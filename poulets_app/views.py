d31dd061267e82701c80c311b2dab85865223d5e
Tenang <medonjiojodeanas@gmail.com>
2025-11-05 02:26:49 +0100

    Ajout du fichier  poulets_app/urls.py,modification de static et visualisation de la page de base

--- poulets_app/views.py
+++ poulets_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def test_base(request):
+    return render(request, 'poulets_app/base.html')
+def dashboard(request):
+    return render(request, 'poulets_app/base.html')
