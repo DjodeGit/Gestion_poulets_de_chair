ad445b538b6e28cefe71035ad4a301a2ee7415bb
Tenang <medonjiojodeanas@gmail.com>
2026-02-16 09:15:09 +0100

    feat: config pour deploiement

--- gestion_poulets/settings.py
+++ gestion_poulets/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,7 +89,9 @@ DATABASES = {
         'PORT': config('DB_PORT'),
     }
 }
-
+DATABASES = {
+    'default': dj_database_url.parse(config('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
